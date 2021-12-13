a3113581307dd41e2864b69e8bc1bdb2f67aea31
Stylish07 <xx3satellite@gmail.com>
2021-12-13 23:05:03 +0900

    프로젝트 시작

---
+++ src/main/java/kr/co/sist/aws/team2/controller/IndexController.java
@@ -0,0 +1,15 @@
+package kr.co.sist.aws.team2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping(value = "/index.do", method = RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
+	
+}
